9fd672f08e79f81c827f38921f6b2a481141f7e1
John Fawcett <jfawcett@cloudflare.com>
2022-10-02 15:53:10 -0500

    Add some errors and timeouts to kitten

--- functions/api/kitten.ts
+++ functions/api/kitten.ts
@@ -1,3 +1,13 @@
-export function onRequest() {
+export async function onRequest() {
+  // randomly error
+  if (Math.random() <= 0.25) {
+    throw new Error("Just a fake exception!");
+  }
+
+  // randomly timeout
+  if (Math.random() <= 0.25) {
+    await new Promise((r) => setTimeout(r, 1000 * 60 * 60));
+  }
+
   return fetch("http://placekitten.com/200/300");
 }
