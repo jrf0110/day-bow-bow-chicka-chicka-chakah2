1eed3f2374511956f49be8038ce4a2178b9f7c34
John Fawcett <jfawcett@cloudflare.com>
2023-01-11 15:26:42 -0600

    WIP

--- _smerker.js
+++ _smerker.js
@@ -1,16 +1,5 @@
 export default {
   fetch(request, env) {
-    const url = new URL(request.url);
-
-    if (url.pathname === "/api/version") {
-      return new Response(
-        JSON.stringify({
-          version: "DAY-BOW-BOW-1",
-        }),
-        { headers: { "Content-Type": "application/json" } }
-      );
-    }
-
     return env.ASSETS.fetch(request);
   },
 };
