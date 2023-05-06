e9cc24d821fcc0180fc52bdbc6ca91fbb3c73ab2
John Fawcett <jfawcett@cloudflare.com>
2023-05-06 14:55:29 -0500

    WIP

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
-# source $HOME/.asdf/asdf.sh
-
-set -e
+set -xe
 
 yarn -v
+yarn -v
+
+locales=($(ls -d ./locales/*))
+
+for locale in "${locales[@]}"; do
+  locale=${locale##*/}
+  mv ./dist/$locale/404/index.html ./dist/$locale/404.html || echo "No 404 page for $locale"
+  rmdir ./dist/$locale/404 || echo "No 404 folder for $locale"
+done
